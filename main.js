58422a361bc4adf24caa3c2b694e99f82184b298
ZZzzaaKK <randufresne@proton.me>
2023-01-01 23:58:39 +0100

    Implement TTS for paragraphs of web page url

--- main.js
+++ main.js
@@ -22,62 +22,141 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const request = __importStar(require("request"));
 const readline = __importStar(require("readline"));
 const fs = __importStar(require("fs"));
-// Replace YOUR_API_KEY with your actual API key
-const API_KEY = "pub_xeantfvrcfxlyoamlz";
-const API_SECRET = "pk_c2d305f3-9026-406c-95cb-ce17640b2b64";
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-rl.question('Enter the text you want to synthesize\n', (text) => {
-    rl.close();
-    const speakOptions = {
-        url: 'https://api.uberduck.ai/speak',
-        auth: {
-            user: API_KEY,
-            pass: API_SECRET,
-        },
-        json: {
-            speech: text,
-            voice: 'zwf',
-        },
-    };
-    request.post(speakOptions, (error, response, body) => {
-        if (error) {
-            console.error(error);
-            return;
-        }
-        console.log(response);
-        console.log(body);
-        const uuid = body.uuid;
-        const speakStatusOptions = {
-            url: `https://api.uberduck.ai/speak-status?uuid=${uuid}`,
+const dotenv = __importStar(require("dotenv"));
+const cheerio = __importStar(require("cheerio"));
+const playsound = require("play-sound");
+dotenv.config();
+const API_KEY = process.env.API_KEY;
+const API_SECRET = process.env.API_SECRET;
+function getUserInput(prompt) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+            terminal: false,
+        });
+        return new Promise((resolve) => {
+            rl.question(`${prompt}`, (text) => {
+                rl.close();
+                resolve(text);
+            });
+        });
+    });
+}
+function getUberduckUuid(text) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const speakOptions = {
+            url: 'https://api.uberduck.ai/speak',
             auth: {
                 user: API_KEY,
                 pass: API_SECRET,
             },
+            json: {
+                speech: text,
+                voice: 'zwf',
+            },
         };
-        const waitForResponse = setInterval(() => {
-            request.get(speakStatusOptions, (error, response, body) => {
+        return new Promise((resolve, reject) => {
+            request.post(speakOptions, (error, response, body) => {
+                console.log(response);
                 if (error) {
-                    console.error(error);
-                    return;
+                    console.log(error);
+                    reject(error);
                 }
-                const parsedBody = JSON.parse(body);
-                console.log(parsedBody);
-                if (parsedBody.path !== null) {
-                    console.log(parsedBody.path);
-                    const audioUrl = parsedBody.path;
-                    const file = fs.createWriteStream("output.wav");
-                    request.get(audioUrl).pipe(file);
-                    clearInterval(waitForResponse);
+                else {
+                    console.log(body);
+                    resolve(body.uuid);
                 }
             });
-        }, 1000);
+        });
     });
+}
+function getParagraphsFromWebpage(url) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return new Promise((resolve, reject) => {
+            try {
+                // Send the HTTP request and get the response body
+                request.get(url, (error, response, body) => {
+                    // Load the HTML into cheerio
+                    const $ = cheerio.load(body);
+                    // Find all paragraph elements and extract their text content
+                    const paragraphs = $('p').map((i, element) => $(element).text()).get();
+                    const text = paragraphs.join(' ');
+                    resolve(text);
+                });
+            }
+            catch (error) {
+                console.log(error);
+                reject(error);
+            }
+        });
+    });
+}
+function getSynthesizedSpeech(uuid) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return new Promise((resolve) => {
+            const waitForResponse = setInterval(() => {
+                console.log(`Checking for response...`);
+                request.get({ url: `https://api.uberduck.ai/speak-status?uuid=${uuid}` }, (error, response, body) => {
+                    if (error) {
+                        console.log(error);
+                        return;
+                    }
+                    console.log(body);
+                    const parsedBody = JSON.parse(body);
+                    console.log(parsedBody);
+                    if (parsedBody.failed_at !== null) {
+                        console.error(`Failed to synthesize speech`);
+                        clearInterval(waitForResponse);
+                    }
+                    if (parsedBody.path !== null) {
+                        clearInterval(waitForResponse);
+                        resolve(parsedBody.path);
+                    }
+                });
+            }, 2000);
+        });
+    });
+}
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
 });
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const url = yield getUserInput('Enter a URL:\n');
+        const text = yield getParagraphsFromWebpage(url);
+        console.log(text);
+        const uuid = yield getUberduckUuid(text);
+        console.log(uuid);
+        if (uuid !== null) {
+            const audioUrl = yield getSynthesizedSpeech(uuid);
+            console.log(`audioURL = ${audioUrl}`);
+            const player = playsound();
+            const file = fs.createWriteStream('output.wav');
+            request.get(audioUrl).pipe(file);
+            file.on('end', () => {
+                player.play('output.wav', (err) => {
+                    if (err) {
+                        console.error(err);
+                    }
+                });
+            });
+        }
+    });
+}
+main();
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -3,20 +3,29 @@ import * as readline from 'readline';
 import * as fs from 'fs';
 import * as dotenv from 'dotenv';
 import * as cheerio from 'cheerio';
+import playsound = require('play-sound');
 
 dotenv.config();
 
 const API_KEY = process.env.API_KEY;
 const API_SECRET = process.env.API_SECRET;
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+async function getUserInput(prompt: string): Promise<string> {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        terminal: false,
+    });
 
-rl.question('Enter the text you want to synthesize', (text: string) => {
-    rl.close();
+    return new Promise((resolve) => {
+        rl.question(`${prompt}`, (text: string) => {
+            rl.close();
+            resolve(text);
+        });
+    });
+}
 
+async function getUberduckUuid(text: string): Promise<string> {
     const speakOptions = {
         url: 'https://api.uberduck.ai/speak',
         auth: {
@@ -29,42 +38,101 @@ rl.question('Enter the text you want to synthesize', (text: string) => {
         },
     };
 
-    request.post(speakOptions, (error, response, body) => {
-        if (error) {
-            console.error(error);
-            return;
+    return new Promise((resolve, reject) => {
+        request.post(speakOptions, (error, response, body) => {
+            console.log(response);
+
+            if (error) {
+                console.log(error);
+                reject(error);
+            } else {
+                console.log(body);
+
+                resolve(body.uuid);
+            }
+        });
+    });
+}
+
+async function getParagraphsFromWebpage(url: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+        try {
+            // Send the HTTP request and get the response body
+            request.get(url, (error, response, body) => {
+                // Load the HTML into cheerio
+                const $ = cheerio.load(body);
+
+                // Find all paragraph elements and extract their text content
+                const paragraphs = $('p').map((i, element) => $(element).text()).get();
+                const text = paragraphs.join(' ');
+
+                resolve(text);
+            });
         }
+        catch (error) {
+            console.log(error);
+            reject(error);
+        }
+    });
+}
 
-        console.log(response);
-        console.log(body);
-
-        const uuid = body.uuid;
-
-        const speakStatusOptions = {
-            url: `https://api.uberduck.ai/speak-status?uuid=${uuid}`,
-            auth: {
-                user: API_KEY,
-                pass: API_SECRET,
-            },
-        };
-
+async function getSynthesizedSpeech(uuid: string): Promise<string> {
+    return new Promise((resolve) => {
         const waitForResponse = setInterval(() => {
-            request.get(speakStatusOptions, (error, response, body) => {
+            console.log(`Checking for response...`);
+            request.get({ url: `https://api.uberduck.ai/speak-status?uuid=${uuid}` }, (error, response, body) => {
                 if (error) {
-                    console.error(error);
+                    console.log(error);
                     return;
                 }
+                console.log(body);
                 const parsedBody = JSON.parse(body);
                 console.log(parsedBody);
 
-                if (parsedBody.path !== null) {
-                    console.log(parsedBody.path);
-                    const audioUrl = parsedBody.path;
-                    const file = fs.createWriteStream("output.wav");
-                    request.get(audioUrl).pipe(file);
+                if (parsedBody.failed_at !== null) {
+                    console.error(`Failed to synthesize speech`);
                     clearInterval(waitForResponse);
                 }
+
+                if (parsedBody.path !== null) {
+                    clearInterval(waitForResponse);
+                    resolve(parsedBody.path);
+                }
             });
-        }, 1000);
+        }, 2000);
     });
+}
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
 });
+
+async function main() {
+    const url = await getUserInput('Enter a URL:\n');
+
+    const text = await getParagraphsFromWebpage(url);
+    console.log(text);
+
+    const uuid = await getUberduckUuid(text);
+    console.log(uuid);
+
+    if (uuid !== null) {
+        const audioUrl = await getSynthesizedSpeech(uuid);
+        console.log(`audioURL = ${audioUrl}`);
+        const player = playsound();
+
+        const file = fs.createWriteStream('output.wav');
+        request.get(audioUrl).pipe(file);
+
+        file.on('end', () => {
+            player.play('output.wav', (err: any) => {
+                if (err) {
+                    console.error('Error playing audio');
+                }
+            });
+        });
+    }
+}
+
+main();
